23cecd52fe138dbda100acca865879df13a5bd6b
czw <chen393804997@qq.com>
2018-10-02 23:11:43 +0800

    初始化

---
+++ brush-ticket/src/main/java/com/czw/brushticket/BrushTicketApplication.java
@@ -0,0 +1,12 @@
+package com.czw.brushticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrushTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrushTicketApplication.class, args);
+    }
+}
